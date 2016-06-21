a985ccdb05adadb1600c4677e4efe74e2f933519
liah-chan <lingzhen.chen@studenti.unitn.it>
2016-06-21 16:33:41 +0200

    vector files

--- negative/gensim_test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from gensim.models import Word2Vec as w2v
-from gensim import models
-import multiprocessing
-
-#model = w2v.load('./gensim_model_1')
-model = w2v.load_word2vec_format('./gensim_model_1', binary=False)
-print model.most_similar('gelatin')
--- window/vec_gen.sh
+++
@@ -1,8 +0,0 @@
-
-for window in {3..32};do
-	time ../word2vec/trunk/word2vec -train ../data/recipes_as_input.txt \
-	-output ./model_${window} -cbow 0 \
-	-size 5 -window ${window}  \
-	-negative 1 -hs 0 -sample 1e-4 -threads 10 \
-	-min-count 120 -binary 0 -iter 15;
-done;
